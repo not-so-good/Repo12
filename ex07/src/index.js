913217c688a8182d92db634cfda76796c583eb68
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-08 19:43:54 +0200

    My music collection

---
+++ ex07/src/index.js
@@ -0,0 +1,47 @@
+var collection = {
+    2458: {
+        album: "Slippery When Wet",
+        artist: "Bon Jovi",
+        tracks: [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    2468: {
+        album: "1999",
+        artist: "Prince",
+        tracks: [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    1245: {
+        artist: "Robert Palmer",
+        tracks: []
+    },
+    5439: {
+        album: "ABBA Gold"
+    }
+};
+
+var oldCollection = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(object, id , prop, value){
+    if(prop !== "tracks" && value !== ""){
+        object[id][prop]= value;
+    } else if(prop === "tracks" && object[id].hasOwnProperty("tracks") === false ){
+        object[id][prop]= [value];
+    } else if(prop === "tracks" && value !== ""){
+        object[id][prop].push(value);
+    } else if(value === ""){
+        delete object[id][prop];
+    }
+    return object;
+}
+
+
+//console.log(updateRecords(collection, 5439, "artist", "ABBA")); 
+//console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
+//console.log(updateRecords(collection, 2458, "artist", "")); //ID broj 2458 je u AC permutiran u 2548
+console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
+module.exports = updateRecords;
